3cc043ba24d79f090bb67d412f6444d123b0a751
Daniel Navajas <daniel.navajas@metafar.io>
2025-04-10 15:45:41 -0300

    added secure endpoint to test out authentication

---
+++ REPRPoc.Application/Secure/Get/V0/EndpointHandler.cs
@@ -0,0 +1,22 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Http;
+namespace REPRPoc.Endpoints.Secure.Get.V0
+{
+    public class EndpointHandler : Endpoint<Request, Response>
+    {
+        public override void Configure()
+        {
+            Post("/secure");
+            Roles("Manager");
+        }
+
+        public override async Task HandleAsync(Request req, CancellationToken ct)
+        {
+            await SendResultAsync(Results.Ok(new Response
+            {
+                FullName = req.FirstName + " " + req.LastName,
+                IsOver18 = req.Age > 18
+            }));
+        }
+    }
+}
---
+++ REPRPoc.Application/Secure/Get/V0/Request.cs
@@ -0,0 +1,9 @@
+﻿namespace REPRPoc.Endpoints.Secure.Get.V0
+{
+    public class Request
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
